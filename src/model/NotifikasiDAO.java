1cdb0e632be1424b963e25e0938be03408a6d9ae
gogopowercoding <furaihanalharits@gmail.com>
2025-06-04 13:58:11 +0700

    ss

--- src/model/NotifikasiDAO.java
+++ src/model/NotifikasiDAO.java
@@ -5,10 +5,10 @@ import java.sql.*;
 public class NotifikasiDAO {
     public void kirimPesanOtomatis() {
         String sql = "INSERT INTO notifikasi (id_user, pesan, tanggal_kirim, status_baca)\n" +
-                     "SELECT u.id, CONCAT('Pajak kendaraan ', k.plat_nomor, ' akan jatuh tempo!'), NOW(), 'belum'\n" +
+                     "SELECT u.id, CONCAT('Pajak kendaraan ', k.nomor_polisi, ' akan jatuh tempo!'), NOW(), 'belum'\n" +
                      "FROM pajak p\n" +
-                     "JOIN kendaraan k ON p.id_kendaraan = k.id\n" +
-                     "JOIN users u ON k.id_user = u.id\n" +
+                     "JOIN kendaraan k ON p.kendaraan_id = k.id\n" +
+                     "JOIN users u ON k.user_id = u.id\n" +
                      "WHERE DATEDIFF(p.jatuh_tempo, NOW()) <= 7 AND p.status = 'BELUM BAYAR'";
 
         try (Connection conn = KoneksidB.getKoneksi()) {
